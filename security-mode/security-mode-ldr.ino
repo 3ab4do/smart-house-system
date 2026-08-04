de474d107c01cab1abb487775d01264a7a4a25d6
abderrahmene omari <omari.abderrahmene@gmail.com>
2026-08-04 23:45:48 +0100

    Fix formatting for laserPin declaration

--- security-mode/security-mode-ldr.ino
+++ security-mode/security-mode-ldr.ino
@@ -1,6 +1,6 @@
 // 1. Pin Definitions
 const int ldrPin = A0;          // Analog input pin for LDR sensor
-const int laserPin = 7;         // Digital output pin to power Laser Module
+const int laserPin = 7;         // Digital output pin to power Laser Module 
 const int armButtonPin = 3;     // Push button to ARM system 
 const int disarmButtonPin = 4;  // Push button to DISARM system 
 const int buzzerPin = 8;        // Digital output pin for alarm buzzer  

a1a9218987531cc125a81bcb56816b21db911d11
abderrahmene omari <omari.abderrahmene@gmail.com>
2026-08-08 23:56:42 +0100

    Update Smart-Energy-saver.ino

--- Smart-Energy-saver/Smart-Energy-saver.ino
+++ Smart-Energy-saver/Smart-Energy-saver.ino
@@ -1,5 +1,5 @@
 // 1. Pin Definitions
-const int pirPin = 2;       // PIR Motion Sensor
+const int pirPin = 2;       // PIR Motion Sensor 
 const int ldrPin = A0;      // LDR Light Sensor 
 const int relayPin = 7;     // Relay Module (Controls Room Light)    
 const int greenLed = 12;    // System Active Indicator     

8bcf12e01f45d9a6f600a8b9a183af45db4133f8
Omari Mohamed Abderrahmane <omari.abderrahmene@gmail.com>
2026-09-17 23:30:14 +0100

    Fix formatting issues in Smart-Energy-saver.ino

--- Smart-Energy-saver/Smart-Energy-saver.ino
+++ Smart-Energy-saver/Smart-Energy-saver.ino
@@ -6,7 +6,7 @@ const int greenLed = 12;    // System Active Indicator
 // 2. Thresholds & Variables
 const int DARK_THRESHOLD = 500; // LDR threshold for darkness    
 unsigned long lastMotionTime = 0;
-const unsigned long LIGHT_TIMEOUT = 10000; // Keep light ON for 10 seconds after motion stops
+const unsigned long LIGHT_TIMEOUT = 10000; // Keep light ON for 10 seconds after motion stops    
 
 void setup() {
   pinMode(pirPin, INPUT);
